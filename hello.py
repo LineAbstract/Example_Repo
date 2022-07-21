99d3d964b50aa8f0c9ea1a9b4422dcb48f462cab
Remo Lanhaus <remo.lanhaus@gmail.com>
2022-07-21 13:26:55 -0700

    New HELLO.py File with items learned including installing python and operating within

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello World")
+# i learned how to: install python, install pipenv, add python to the PATH in Windows, check version installed through CMD (python --version), installed python extension in vscode, created a python file ending in .py, selected a python interpreter (ctrl+shift+p > python:inter), how to run file through play button, how to debug through F5
+# i now also know how to gitbash this local file to my github repo
+# next, i want to learn how to add a line break to more clearly see the entirety of what's being written
